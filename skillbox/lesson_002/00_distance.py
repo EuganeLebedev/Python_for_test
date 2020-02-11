b4207aba82866d6599bf25d552d6578076d4edae
Eugane Lebedev <e.lebedev@grosver.com>
2020-02-11 09:38:20 +0300

    Task 00. Distances between citys

--- skillbox/lesson_002/00_distance.py
+++ skillbox/lesson_002/00_distance.py
@@ -17,10 +17,14 @@ distances = {}
 # TODO здесь заполнение словаря
 for town in sites:
     sites_temp = sites.copy()
-    sites_temp.pop(town) 
-#    distence = (sites_temp[0][0] - sites_temp[1][0]) ** 2
-#    + (sites_temp[1][0] - sites_temp[1][1]) ** 2
-#    distances.update({1: 1})
-    print(list(sites_temp.values())[0][0])
-
+    sites_temp.pop(town)
+    towns_between = list((sites_temp.keys()))[0] + \
+                        '_' + list((sites_temp.keys()))[1]
+    x1 = int(list(sites_temp.values())[0][0])
+    x2 = int(list(sites_temp.values())[1][0])
+    y1 = int(list(sites_temp.values())[0][1])
+    y2 = int(list(sites_temp.values())[1][1])
+    result = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    distances.update({towns_between: int(result)})
 print(distances)
+
