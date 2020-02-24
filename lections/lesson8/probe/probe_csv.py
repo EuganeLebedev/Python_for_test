ec24c08f3b141dcdf8730fd3c29245d983a1abbc
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-02-24 20:01:53 +0300

    move lection 8 probe to another folder probe csv file probe xlsx file

--- lections/lesson8/probe/probe_csv.py
+++ lections/lesson8/probe/probe_csv.py
@@ -1,7 +1,7 @@
 import csv
 
 
-with open('OITM.csv', encoding='utf-8') as oitm:
+with open('../../../OITM.csv', encoding='utf-8') as oitm:
     csv_reader = csv.DictReader(oitm, delimiter=';')
     for row in csv_reader:
         print(row['ItemCode   '])
