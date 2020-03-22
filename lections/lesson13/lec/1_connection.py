2ea2a03242c47d6146bf6f790b702d094eb28de3
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-03-22 15:41:48 +0300

    lessons 13 and 14 code

---
+++ lections/lesson13/lec/1_connection.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql+psycopg2://postgres@localhost/postgres', echo=True)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    name = Column(String, primary_key=True)
+    lastname = Column(String)
+
+Base.metadata.create_all(engine)
+
+user1 = User(name="Ivan", lastname="Petrov")
+user1.name = "John"
+print(user1.name)
+print(user1.lastname)
+
