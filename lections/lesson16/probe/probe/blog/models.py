90c259510f3dd326892e752322c1eb176197de34
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-04-07 22:32:48 +0300

    Добавил ключи в структуру моделей блога

--- lections/lesson16/probe/probe/blog/models.py
+++ lections/lesson16/probe/probe/blog/models.py
@@ -11,18 +11,36 @@ class Post(models.Model):
     create_date = models.DateTimeField(auto_now_add=True) 
     update_date = models.DateTimeField(auto_now=True) 
 
+    def __str__(self):
+        return f'{self.id} {self.title}'
+
 class Author(models.Model):
     nickname = models.CharField(max_length=50, null=False, primary_key=True)
     user_name = models.CharField(max_length=30)
     user_lastname = models.CharField(max_length=30)
-    user_email = models.CharField(max_length=30)
+    user_email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return f'{self.nickname}'
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     comment = models.TextField() 
     create_date = models.DateTimeField(auto_now_add=True) 
     update_date = models.DateTimeField(auto_now=True) 
 
+    @property
+    def short_comment(self):
+        return self.comment[:25]
+
+
+    def __str__(self):
+        return f'{self.author} to post: {self.post} {self.short_comment}'
+
 class Topic(models.Model):
-    topic = models.CharField(max_length=25)
+    topic = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return f'{self.topic}'
 
